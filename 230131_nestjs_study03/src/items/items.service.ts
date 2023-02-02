6c44d640c9fb4ed9a9df24e6f3c126d37eec7e93
Ren Ichinose <kamatani.toru@gmail.com>
2023-02-03 03:00:58 +0900

    CRUDの復習2 #3

--- 230131_nestjs_study03/src/items/items.service.ts
+++ 230131_nestjs_study03/src/items/items.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { ItemStatus } from './item-status.enum';
+import { Item } from './item.model';
 
 @Injectable()
 export class ItemsService {
+    private items: Item[] = [];
+    
     findAll(){
-        return 'This is ItemsService'
+        return this.items;
     }
+
+    findById(id: string): Item {
+        return this.items.find( item => item.id === id );
+    }
+
+    creat(item: Item): Item {
+        this.items.push(item);
+        return item;
+    }
+
+    updateStatus(id: string): Item {
+        const item = this.findById(id);
+        item.status = 
+            item.status === ItemStatus.ON_SALE ?
+            ItemStatus.SOLD_OUT :
+            ItemStatus.ON_SALE;
+        return item;
+    }
+
+    delete(id: string): void {
+        this.items = this.items.filter( item => item.id !== id );
+    }
+
 }
