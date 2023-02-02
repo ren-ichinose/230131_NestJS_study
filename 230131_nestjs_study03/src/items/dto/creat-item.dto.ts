fea776b85c2487580460112f5623cef7345e5852
Ren Ichinose <kamatani.toru@gmail.com>
2023-02-03 03:16:29 +0900

    DTOの学習 #4

---
+++ 230131_nestjs_study03/src/items/dto/creat-item.dto.ts
@@ -0,0 +1,6 @@
+export class CreatItemDto {
+    id: string;
+    name: string;
+    price: number;
+    description: string;
+}
--- 230131_nestjs_study03/src/items/items.service.ts
+++ 230131_nestjs_study03/src/items/items.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { CreatItemDto } from './dto/creat-item.dto';
 import { ItemStatus } from './item-status.enum';
 import { Item } from './item.model';
 
@@ -14,7 +15,11 @@ export class ItemsService {
         return this.items.find( item => item.id === id );
     }
 
-    creat(item: Item): Item {
+    creat(creatItemDto: CreatItemDto): Item {
+        const item = {
+            ...creatItemDto,
+            status: ItemStatus.ON_SALE,
+        }
         this.items.push(item);
         return item;
     }
