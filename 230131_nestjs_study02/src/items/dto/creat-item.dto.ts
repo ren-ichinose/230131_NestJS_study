b6a8a7343ffaecdd06382a0a60c3a9dc02d39ede
Ren Ichinose <kamatani.toru@gmail.com>
2023-02-04 03:26:52 +0900

    バリデーションの復習2 #4

--- 230131_nestjs_study02/src/items/dto/creat-item.dto.ts
+++ 230131_nestjs_study02/src/items/dto/creat-item.dto.ts
@@ -1,5 +1,18 @@
+import { Type } from "class-transformer";
+import { IsInt, IsNotEmpty, IsString, MaxLength, Min } from "class-validator";
+
 export class CreatItemDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(32)
     name: string;
+
+    @IsInt()
+    @Min(1)
+    @Type(() => Number)
     price: number;
+
+    @IsString()
+    @IsNotEmpty()
     description: string;
 }
--- 230131_nestjs_study02/src/items/item.controller.ts
+++ 230131_nestjs_study02/src/items/item.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { CreatItemDto } from './dto/creat-item.dto';
 import { Item } from './items.model';
 import { ItemsService } from './items.service';
@@ -13,7 +13,7 @@ export class ItemsController {
     }
 
     @Get(':id')
-    findById(@Param('id') id: string): Item {
+    findById(@Param('id', ParseUUIDPipe) id: string): Item {
         return this.itemsService.findById(id);
     }
 
@@ -23,12 +23,12 @@ export class ItemsController {
     }
 
     @Patch(':id')
-    updateStatus (@Param('id') id: string): Item {
+    updateStatus (@Param('id', ParseUUIDPipe) id: string): Item {
         return this.itemsService.updateStatus(id);
     }
 
     @Delete(':id')
-    delete(@Param('id') id: string): void {
+    delete(@Param('id', ParseUUIDPipe) id: string): void {
         return this.itemsService.delete(id);
     }
 }
