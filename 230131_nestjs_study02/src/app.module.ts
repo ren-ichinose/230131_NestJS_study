0f9a6edbe3c4b4c9e7dfb998ab54fe626b39933f
Ren Ichinose <kamatani.toru@gmail.com>
2023-02-04 06:16:27 +0900

    TypeORMの設定 #6

--- 230131_nestjs_study02/src/app.module.ts
+++ 230131_nestjs_study02/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ItemsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',// docker-compose.yml で指定したコンテナ名
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
