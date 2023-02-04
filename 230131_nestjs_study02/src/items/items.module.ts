f57e138723c6db766a137ea79e917fde58e10e43
Ren Ichinose <kamatani.toru@gmail.com>
2023-02-04 23:15:52 +0900

    ormconfigへの切り出しとリポジトリの実装 #6

--- 230131_nestjs_study02/src/items/items.module.ts
+++ 230131_nestjs_study02/src/items/items.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { ItemsController } from './item.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ItemRepository } from './imte.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ItemRepository])],
   providers: [ItemsService],
   controllers: [ItemsController]
 })
