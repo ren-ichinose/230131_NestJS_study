d65648a490ee2f807deb68b3b0fa95594c70bb47
Ren Ichinose <kamatani.toru@gmail.com>
2023-02-05 19:10:33 +0900

    SignUp機能の実装 #7

--- 230131_nestjs_study01/src/app.module.ts
+++ 230131_nestjs_study01/src/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsModule } from './items/items.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot(),
     ItemsModule,
-    TypeOrmModule.forRoot()],
+    AuthModule],
   controllers: [],
   providers: [],
 })
