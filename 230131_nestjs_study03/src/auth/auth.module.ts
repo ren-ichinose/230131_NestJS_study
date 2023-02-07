d43680925c77bb3ee7caeca9f0008415355cd50a
Ren Ichinose <kamatani.toru@gmail.com>
2023-02-08 05:12:28 +0900

    SignUp機能の実装 #7

--- 230131_nestjs_study03/src/auth/auth.module.ts
+++ 230131_nestjs_study03/src/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: 3600 }
+    })
+],
   providers: [AuthService],
   controllers: [AuthController]
 })
