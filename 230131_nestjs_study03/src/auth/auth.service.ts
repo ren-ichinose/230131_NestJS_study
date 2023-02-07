b6f9d44f09bcf8c833053f957fe1f3090ad32861
Ren Ichinose <kamatani.toru@gmail.com>
2023-02-08 05:34:50 +0900

    ログイン機能の実装の復習 #7

--- 230131_nestjs_study03/src/auth/auth.service.ts
+++ 230131_nestjs_study03/src/auth/auth.service.ts
@@ -1,13 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { User } from 'src/entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import { CredentialsDto } from './dto/credentials.dto';
 import { UserRepository } from './user.repository';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userRepository: UserRepository) {}
+    constructor(
+        private readonly userRepository: UserRepository,
+        private readonly jwtService: JwtService
+        ) {}
 
     async signUp(createUserDto: CreateUserDto): Promise<User> {
         return await this.userRepository.createUser(createUserDto);
+    }
+
+    async signIn(credentialsDto: CredentialsDto): Promise<{ accessToken: string}> {
+        const { username, password } = credentialsDto;
+        const user = await this.userRepository.findOne({ username });
+
+        if(user && (await bcrypt.compare(password, user.password))) {
+            const payload = { id: user.id, username: user.username };
+            const accessToken = this.jwtService.sign(payload);
+            return { accessToken };
+        }
+        throw new UnauthorizedException('ユーザー名またはパスワードを確認してください');
     }
 }
