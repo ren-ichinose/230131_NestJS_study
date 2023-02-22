fec7b4ff7347586a62d2b38e020a5fa902a26099
Ren Ichinose <kamatani.toru@gmail.com>
2023-02-22 23:21:58 +0900

    データベースの復習 #6

--- 230131_nestjs_study04/src/app.module.ts
+++ 230131_nestjs_study04/src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsModule } from './items/items.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(),
+    ItemsModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
