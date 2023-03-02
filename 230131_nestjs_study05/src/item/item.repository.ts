f074b8eb3f21f2b47f119eab4cfe8064f966d8f8
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-02 23:51:46 +0900

    ItemRepositoryの実装

---
+++ 230131_nestjs_study05/src/item/item.repository.ts
@@ -0,0 +1,23 @@
+import { Item } from "src/entities/item.entity";
+import { User } from "src/entities/user.entity";
+import { EntityRepository, Repository } from "typeorm";
+import { CreateItemDto } from "./dto/create-item.dto";
+import { ItemStatus } from "./item-status.enum";
+
+@EntityRepository(Item)
+export class ItemRepository extends Repository<Item> {
+  async createItem(createItemDto: CreateItemDto, user: User): Promise<Item> {
+    const { name, price, description } = createItemDto;
+    const item = this.create({
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+      userId: user.id,
+    });
+    await this.save(item)
+    return item;
+  }
+}
