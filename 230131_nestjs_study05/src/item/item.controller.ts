8f7c398c8abe1102a2f8bd6828beb31ee9ed078b
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-04 20:50:59 +0900

    Itemクラスの実装

--- 230131_nestjs_study05/src/item/item.controller.ts
+++ 230131_nestjs_study05/src/item/item.controller.ts
@@ -1,21 +1,65 @@
-import { Body, Controller, Post, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { GetUser } from 'src/auth/decorator/get-decorator';
+import { Role } from 'src/auth/decorator/role.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { UserStatus } from 'src/auth/user-status.enum';
 import { Item } from 'src/entities/item.entity';
 import { User } from 'src/entities/user.entity';
 import { CreateItemDto } from './dto/create-item.dto';
 import { ItemService } from './item.service';
 
 @Controller('item')
+@UseInterceptors(ClassSerializerInterceptor)
 export class ItemController {
   constructor(private readonly itemService: ItemService) {}
 
+  @Get()
+  async findAll(): Promise<Item[]> {
+    return await this.itemService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id', ParseUUIDPipe) id: string): Promise<Item> {
+    return await this.itemService.findById(id);
+  }
   @Post()
-  @UseGuards(JwtAuthGuard)
+  @Role(UserStatus.PREMIUM)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   async createItem(
     @Body() createItemDto: CreateItemDto,
     @GetUser() user: User,
   ): Promise<Item> {
     return await this.itemService.createItem(createItemDto, user);
+  }
+
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  async updateStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @GetUser() user: User,
+  ): Promise<Item> {
+    return await this.itemService.updateStatus(id, user);
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  async deleteItem(
+    @Param('id', ParseUUIDPipe) id: string,
+    @GetUser() user: User,
+  ): Promise<void> {
+    await this.itemService.deleteItem(id, user);
   }
 }
--- 230131_nestjs_study05/src/item/item.service.ts
+++ 230131_nestjs_study05/src/item/item.service.ts
@@ -1,14 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { Item } from 'src/entities/item.entity';
 import { User } from 'src/entities/user.entity';
 import { CreateItemDto } from './dto/create-item.dto';
 import { ItemRepository } from './item.repository';
+import { ItemStatus } from './item-status.enum';
 
 @Injectable()
 export class ItemService {
   constructor(private readonly itemRepository: ItemRepository) {}
 
+  async findAll(): Promise<Item[]> {
+    return await this.itemRepository.find();
+  }
+
+  async findById(id: string): Promise<Item> {
+    const item = await this.itemRepository.findOne(id);
+    if (!item) {
+      throw new NotFoundException({
+        statusCode: 404,
+        message: '商品が見つかりませんでした',
+      });
+    }
+    return item;
+  }
+
   createItem(createItemDto: CreateItemDto, user: User): Promise<Item> {
     return this.itemRepository.createItem(createItemDto, user);
+  }
+
+  async updateStatus(id: string, user: User): Promise<Item> {
+    const item = await this.findById(id);
+    if (user.id === item.userId) {
+      throw new BadRequestException('この商品の購入はできません');
+    }
+    const updateStatusItem = {
+      ...item,
+      status: ItemStatus.SOLD_OUT,
+      updatedAt: new Date().toISOString(),
+    };
+    await this.itemRepository.save(updateStatusItem);
+    return updateStatusItem;
+  }
+
+  async deleteItem(id: string, user: User): Promise<void> {
+    const item = await this.findById(id);
+    if (item.userId !== user.id) {
+      throw new BadRequestException('この商品の削除はできません');
+    }
+    await this.itemRepository.delete({ id });
   }
 }
